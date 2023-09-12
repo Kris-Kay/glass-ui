3ea547827239d6e66d7c6484bc29186f867960f5
Kris Kay <kris.kay.design@gmail.com>
2023-09-11 23:47:08 -0400

    html bits

--- src/js/main.js
+++ src/js/main.js
@@ -25,13 +25,12 @@ itemsArray.forEach((item) => {
   };
 });
 
-
 // Tab menu for switching between backgrounds
 /* ================================== */
 const $tabMenu = document.getElementById("js-tabMenu");
 const $bg = document.getElementById("js-bg");
 const $color = document.getElementById("color-tab");
-let   $prevTab = $color;
+let $prevTab = $color;
 
 buttonPressed = (e) => {
   if (e.target.nodeName === "BUTTON") {
@@ -82,10 +81,6 @@ buttonPressed = (e) => {
 
 $tabMenu.addEventListener("click", buttonPressed);
 
-
-
-
-
 /* Accordion */
 /* ================================== */
 //uses classList, setAttribute, and querySelectorAll
@@ -94,74 +89,75 @@ $tabMenu.addEventListener("click", buttonPressed);
 // const toggleArray = Array.from(accordionToggles);
 // console.log(toggleArray);
 
-(function(){
-	let accordionToggles = document.querySelectorAll('.js-accordionToggle'),
-	// setAria,
-	setAccordionAria,
-	switchAccordion,
-  touchSupported = ('ontouchstart' in window),
-  pointerSupported = ('pointerdown' in window);
-  
-  skipClickDelay = function(e){
+(function () {
+  let accordionToggles = document.querySelectorAll(".js-accordionToggle"),
+    // setAria,
+    setAccordionAria,
+    switchAccordion,
+    touchSupported = "ontouchstart" in window,
+    pointerSupported = "pointerdown" in window;
+
+  skipClickDelay = function (e) {
     e.preventDefault();
     e.target.click();
   };
 
-	setAriaAttr = function(el, ariaType, newProperty){
-		el.setAttribute(ariaType, newProperty);
-	};
+  setAriaAttr = function (el, ariaType, newProperty) {
+    el.setAttribute(ariaType, newProperty);
+  };
 
-	setAccordionAria = function(el1, el2, expanded){
-		switch(expanded) {
+  setAccordionAria = function (el1, el2, expanded) {
+    switch (expanded) {
       case "true":
-      	setAriaAttr(el1, 'aria-expanded', 'true');
-      	setAriaAttr(el2, 'aria-hidden', 'false');
-      	break;
+        setAriaAttr(el1, "aria-expanded", "true");
+        setAriaAttr(el2, "aria-hidden", "false");
+        break;
       case "false":
-      	setAriaAttr(el1, 'aria-expanded', 'false');
-      	setAriaAttr(el2, 'aria-hidden', 'true');
-      	break;
+        setAriaAttr(el1, "aria-expanded", "false");
+        setAriaAttr(el2, "aria-hidden", "true");
+        break;
       default:
-				break;
-		}
-	};
-
-//function
-switchAccordion = function(e) {
-  
-	e.preventDefault();
-
-	var thisContent = e.target.parentNode.nextElementSibling;
-	var thisAccordion = e.target.parentNode.parentNode;
-  console.log(thisAccordion);
-	var thisToggle = e.target;
-	if(thisContent.classList.contains('is-closed')) {
-		setAccordionAria(thisToggle, thisContent, 'true');
-	} else {
-		setAccordionAria(thisToggle, thisContent, 'false');
-	}
-  	thisToggle.classList.toggle('is-closed');
-  	thisToggle.classList.toggle('is-open');
-		thisContent.classList.toggle('is-closed');
-		thisContent.classList.toggle('is-open');
-    thisAccordion.classList.toggle('is-closed');
-		thisAccordion.classList.toggle('is-open');
- 	
-  	// thisContent.classList.toggle('animateIn');
-	};
-
-	for (var i=0,len=accordionToggles.length; i<len; i++) {
-		if(touchSupported) {
-      accordionToggles[i].addEventListener('touchstart', skipClickDelay, false);
+        break;
     }
-    if(pointerSupported){
-      accordionToggles[i].addEventListener('pointerdown', skipClickDelay, false);
+  };
+
+  //function
+  switchAccordion = function (e) {
+    e.preventDefault();
+
+    var thisContent = e.target.parentNode.nextElementSibling;
+    var thisAccordion = e.target.parentNode.parentNode;
+    console.log(thisAccordion);
+    var thisToggle = e.target;
+    if (thisContent.classList.contains("is-closed")) {
+      setAccordionAria(thisToggle, thisContent, "true");
+    } else {
+      setAccordionAria(thisToggle, thisContent, "false");
     }
-    accordionToggles[i].addEventListener('click', switchAccordion, false);
+    thisToggle.classList.toggle("is-closed");
+    thisToggle.classList.toggle("is-open");
+    thisContent.classList.toggle("is-closed");
+    thisContent.classList.toggle("is-open");
+    thisAccordion.classList.toggle("is-closed");
+    thisAccordion.classList.toggle("is-open");
+
+    // thisContent.classList.toggle('animateIn');
+  };
+
+  for (var i = 0, len = accordionToggles.length; i < len; i++) {
+    if (touchSupported) {
+      accordionToggles[i].addEventListener("touchstart", skipClickDelay, false);
+    }
+    if (pointerSupported) {
+      accordionToggles[i].addEventListener(
+        "pointerdown",
+        skipClickDelay,
+        false
+      );
+    }
+    accordionToggles[i].addEventListener("click", switchAccordion, false);
   }
 })();
-
-
 
 /*  Card hover effect */
 /* ================================== */
@@ -186,10 +182,6 @@ switchAccordion = function(e) {
 //   e.target.style.setProperty("--x", `${x}px`);
 //   e.target.style.setProperty("--y", `${y}px`);
 // };
-
-
-
-
 
 // Dynamic border radius
 /* ================================== */
---
+++ src/js/parts.js
@@ -0,0 +1,20 @@
+/* ================================== */
+/* jQuary load bits */
+/* ================================== */
+
+(function () {
+  "use strict";
+
+  $(function () {
+    $(".js-nav").load("/nav.html");
+  });
+})();
+
+
+(function () {
+  "use strict";
+
+  $(function () {
+    $(".js-footer").load("/./html/footer.html");
+  });
+})();
