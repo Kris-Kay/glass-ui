f9e83b5b48b81d5539ea2843d773131c10dec288
Kris Kay <kris.kay.design@gmail.com>
2023-09-21 19:42:17 -0400

    js

--- src/js/content.js
+++ src/js/content.js
@@ -9,22 +9,31 @@
     $("#content").load("/home.html");
     // $("#content").load("/tutorial.html");
 
-    $("#js-demo").on( "click", function(event) {
+    $(".js-demo").on( "click", function(event) {
       event.preventDefault();
-      $(window).scrollTop(0);
+      // $(window).scrollTop(0);
       $("#content").load("/demo.html");
     }); 
 
-    $("#js-tutorial").on( "click", function(event) {
+    $(".js-tutorial").on( "click", function(event) {
       event.preventDefault();
-      $(window).scrollTop(0);
+      // $(window).scrollTop(0);
       $("#content").load("/tutorial.html");
     }); 
 
-    $("#js-home").on( "click", function(event) {
+    $(".js-home").on( "click", function(event) {
       event.preventDefault();
-      $(window).scrollTop(0);
+      // $(window).scrollTop(0);
       $("#content").load("/home.html");
     });
   });
 })();
+
+
+  $(function () {
+    $("#js-tutorial-two").on( "click", function(event) {
+      event.preventDefault();
+      // $(window).scrollTop(0);
+      $("#content").load("/tutorial.html");
+    }); 
+  });
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 /* ================================== */
 /* JS */
 /* ================================== */
-  // console.log("page is fully loaded");
 
 // Open & Close dropdown
 /* ================================== */
@@ -90,77 +89,77 @@ $tabMenu.addEventListener("click", buttonPressed);
 // const toggleArray = Array.from(accordionToggles);
 // console.log(toggleArray);
 
-(function () {
-  let accordionToggles = document.querySelectorAll(".js-accordionToggle"),
-    // setAria,
-    setAccordionAria,
-    switchAccordion,
-    touchSupported = "ontouchstart" in window,
-    pointerSupported = "pointerdown" in window;
+// (function () {
+//   let accordionToggles = document.querySelectorAll(".js-accordionToggle"),
+//     // setAria,
+//     setAccordionAria,
+//     switchAccordion,
+//     touchSupported = "ontouchstart" in window,
+//     pointerSupported = "pointerdown" in window;
 
-  skipClickDelay = function (e) {
-    e.preventDefault();
-    e.target.click();
-  };
+//   skipClickDelay = function (e) {
+//     e.preventDefault();
+//     e.target.click();
+//   };
 
-  setAriaAttr = function (el, ariaType, newProperty) {
-    el.setAttribute(ariaType, newProperty);
-  };
+//   setAriaAttr = function (el, ariaType, newProperty) {
+//     el.setAttribute(ariaType, newProperty);
+//   };
 
-  setAccordionAria = function (el1, el2, expanded) {
-    switch (expanded) {
-      case "true":
-        setAriaAttr(el1, "aria-expanded", "true");
-        setAriaAttr(el2, "aria-hidden", "false");
-        break;
-      case "false":
-        setAriaAttr(el1, "aria-expanded", "false");
-        setAriaAttr(el2, "aria-hidden", "true");
-        break;
-      default:
-        break;
-    }
-  };
+//   setAccordionAria = function (el1, el2, expanded) {
+//     switch (expanded) {
+//       case "true":
+//         setAriaAttr(el1, "aria-expanded", "true");
+//         setAriaAttr(el2, "aria-hidden", "false");
+//         break;
+//       case "false":
+//         setAriaAttr(el1, "aria-expanded", "false");
+//         setAriaAttr(el2, "aria-hidden", "true");
+//         break;
+//       default:
+//         break;
+//     }
+//   };
 
-  //function
-  switchAccordion = function (e) {
-    e.preventDefault();
+//   //function
+//   switchAccordion = function (e) {
+//     e.preventDefault();
 
-    var thisContent = e.target.parentNode.nextElementSibling;
-    var thisAccordion = e.target.parentNode.parentNode;
-    console.log(thisAccordion);
-    var thisToggle = e.target;
-    if (thisContent.classList.contains("is-closed")) {
-      setAccordionAria(thisToggle, thisContent, "true");
-    } else {
-      setAccordionAria(thisToggle, thisContent, "false");
-    }
-    thisToggle.classList.toggle("is-closed");
-    thisToggle.classList.toggle("is-open");
-    thisContent.classList.toggle("is-closed");
-    thisContent.classList.toggle("is-open");
-    thisAccordion.classList.toggle("is-closed");
-    thisAccordion.classList.toggle("is-open");
+//     var thisContent = e.target.parentNode.nextElementSibling;
+//     var thisAccordion = e.target.parentNode.parentNode;
+//     console.log(thisAccordion);
+//     var thisToggle = e.target;
+//     if (thisContent.classList.contains("is-closed")) {
+//       setAccordionAria(thisToggle, thisContent, "true");
+//     } else {
+//       setAccordionAria(thisToggle, thisContent, "false");
+//     }
+//     thisToggle.classList.toggle("is-closed");
+//     thisToggle.classList.toggle("is-open");
+//     thisContent.classList.toggle("is-closed");
+//     thisContent.classList.toggle("is-open");
+//     thisAccordion.classList.toggle("is-closed");
+//     thisAccordion.classList.toggle("is-open");
 
-    // thisContent.classList.toggle('animateIn');
-  };
+//     // thisContent.classList.toggle('animateIn');
+//   };
 
-  for (var i = 0, len = accordionToggles.length; i < len; i++) {
-    if (touchSupported) {
-      accordionToggles[i].addEventListener(
-        "touchstart",
-        skipClickDelay,
-        false
-      );
-    }
-    if (pointerSupported) {
-      accordionToggles[i].addEventListener(
-        "pointerdown",
-        skipClickDelay,
-        false
-      );
-    }
-    accordionToggles[i].addEventListener("click", switchAccordion, false);
-  }
-})();
+//   for (var i = 0, len = accordionToggles.length; i < len; i++) {
+//     if (touchSupported) {
+//       accordionToggles[i].addEventListener(
+//         "touchstart",
+//         skipClickDelay,
+//         false
+//       );
+//     }
+//     if (pointerSupported) {
+//       accordionToggles[i].addEventListener(
+//         "pointerdown",
+//         skipClickDelay,
+//         false
+//       );
+//     }
+//     accordionToggles[i].addEventListener("click", switchAccordion, false);
+//   }
+// })();
 
