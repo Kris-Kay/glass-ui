e80e7f798466f5a5ab70bfca079710a673f27909
Kris Kay <kris.kay.design@gmail.com>
2023-09-27 20:33:09 -0400

    moving copy around

--- src/js/content.js
+++ src/js/content.js
@@ -6,8 +6,8 @@
   "use strict";
 
   $(function () {
-    // $("#content").load("/home.html");
-    $("#content").load("/about.html");
+    $("#content").load("/home.html");
+    // $("#content").load("/about.html");
     // $("#content").load("/tutorial.html");
 
     $(".js-demo").on( "click", function(event) {
