bc2f8454434e5dc62fe9f9cb205fca93b0c8ef0e
Kris Kay <kris.kay.design@gmail.com>
2023-10-03 22:59:36 -0400

    see dynamic sizing

--- src/js/demo.js
+++ src/js/demo.js
@@ -2,6 +2,32 @@
 /* JS */
 /* ================================== */
 
+/* Responsive size range print out */
+/* ================================== */
+let timeout = false; // holder for timeout id
+let delay = 100; // delay after event is "complete" to run callback
+
+function cubeWidth() {
+  const $allCubes = document.querySelectorAll(".js-sizeCube");
+  const $cubesArray = Array.from($allCubes);
+
+  $cubesArray.forEach((cube) => {
+    let $w = cube.offsetWidth;
+    let $span = cube.nextElementSibling.nextElementSibling;
+    $span.innerText = `${$w}px`;
+  });
+}
+
+// window.resize event listener
+window.addEventListener("resize", function (event) {
+  clearTimeout(timeout);
+  timeout = setTimeout(cubeWidth, delay);
+});
+
+cubeWidth();
+
+
+
 // Open & Close dropdown
 /* ================================== */
 // const $ham = document.getElementById("js-navHam");
