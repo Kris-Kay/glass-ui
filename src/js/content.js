a91fa976f0166c621621c5d2700e913953074ee9
Kris Kay <kris.kay.design@gmail.com>
2023-10-06 15:36:03 -0400

    rename

--- src/js/content.js
+++ src/js/content.js
@@ -7,26 +7,32 @@
 
   $(function () {
     // $("#content").load("/home.html");
-    // $("#content").load("/about.html");
-    $("#content").load("/demo.html");
+    $("#content").load("/about.html");
+    // $("#content").load("/demo.html");
     // $("#content").load("/tutorial.html");
 
-    $(".js-demo").on( "click", function(event) {
+    $("#js-home").on( "click", function(event) {
       event.preventDefault();
-      // $(window).scrollTop(0);
+      $(window).scrollTop(0);
+      $("#content").load("/home.html");
+    });
+
+    $("#js-about").on( "click", function(event) {
+      event.preventDefault();
+      $(window).scrollTop(0);
+      $("#content").load("/about.html");
+    }); 
+
+    $("#js-demo").on( "click", function(event) {
+      event.preventDefault();
+      $(window).scrollTop(0);
       $("#content").load("/demo.html");
     }); 
 
-    $(".js-tutorial").on( "click", function(event) {
-      event.preventDefault();
-      // $(window).scrollTop(0);
-      $("#content").load("/tutorial.html");
-    }); 
-
-    $(".js-home").on( "click", function(event) {
-      event.preventDefault();
-      // $(window).scrollTop(0);
-      $("#content").load("/home.html");
-    });
+    // $("#js-tutorial").on( "click", function(event) {
+    //   event.preventDefault();
+    //   $(window).scrollTop(0);
+    //   $("#content").load("/tutorial.html");
+    // }); 
   });
 })();
