c3d676722c7340c3f496e4c097c22305f68da6dd
Kris Kay <kris.kay.design@gmail.com>
2023-10-07 16:05:39 -0400

    js file split

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -2,28 +2,28 @@
 /* Accordion JS */
 /* ================================== */
 
-let accordionToggles = document.querySelectorAll(".js-accordionToggle");
-let toggleArray = Array.from(accordionToggles);
-console.log(toggleArray);
+let $accordionToggles = document.querySelectorAll(".js-accordionToggle");
+let $toggleArray = Array.from($accordionToggles);
+console.log($toggleArray);
 
 toggleAccordion = (e) => {
   e.preventDefault();
 
-  let thisToggle = e.target;
+  let $thisToggle = e.target;
 
-  let thisAccordionHeader = e.target.parentNode;
+  let $thisHeader = e.target.parentNode.parentNode;
 
-  let thisContent = thisToggle.parentNode.nextElementSibling;
+  let $thisContent = $thisHeader.nextElementSibling;
 
-  console.log(thisToggle);
-  console.log(thisAccordionHeader);
-  console.log(thisContent);
+  console.log($thisToggle);
+  console.log($thisHeader);
+  console.log($thisContent);
   
-  thisToggle.classList.toggle("is-closed");
-  thisAccordionHeader.classList.toggle("is-closed");;
-  thisContent.classList.toggle("is-closed");
+  $thisToggle.classList.toggle("is-closed");
+  $thisHeader.classList.toggle("is-closed");;
+  $thisContent.classList.toggle("is-closed");
 };
 
-toggleArray.forEach((toggle) => {
+$toggleArray.forEach((toggle) => {
   toggle.addEventListener("click", toggleAccordion);
 });
