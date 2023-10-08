e70db7dc05fbd4f5bc4cf5094e2233d7d48d1031
Kris Kay <kris.kay.design@gmail.com>
2023-10-08 15:02:59 -0400

    copy js

--- public/js/copyToClipboard.js
+++ public/js/copyToClipboard.js
@@ -1,44 +1,69 @@
 /* ================================== */
-/* Tutorial JS */
-/* ================================== */
-
-/* Accordion Toggles */
-/* ================================== */
-let accordionToggles = document.querySelectorAll(".js-accordionToggle");
-let toggleArray = Array.from(accordionToggles);
-console.log(toggleArray);
-
-toggleAccordion = (e) => {
-  e.preventDefault();
-
-  let thisToggle = e.target;
-
-  let thisAccordionHeader = e.target.parentNode;
-
-  let thisContent = thisToggle.parentNode.nextElementSibling;
-
-  console.log(thisToggle);
-  console.log(thisAccordionHeader);
-  console.log(thisContent);
-  
-  thisToggle.classList.toggle("is-closed");
-  thisAccordionHeader.classList.toggle("is-closed");;
-  thisContent.classList.toggle("is-closed");
-};
-
-toggleArray.forEach((toggle) => {
-  toggle.addEventListener("click", toggleAccordion);
-});
-
-
 /* Copy to clipboard */
 /* ================================== */
-let codeText = document.querySelector('#code-text');
-let handleCopyClick = document.querySelector('#copy-code');
 
-handleCopyClick.addEventListener('click', () => {
-	let text = codeText.textContent;
-	navigator.clipboard.writeText(`${text}`);
+// let codeText = document.querySelector("#code-text");
+// let handleCopyClick = document.querySelector('#copy-code');
 
-	alert(`Code copied to clipboard!`);
+// handleCopyClick.addEventListener('click', () => {
+// 	let text = codeText.textContent;
+// 	navigator.clipboard.writeText(`${text}`);
+
+// 	alert(`Code copied to clipboard!`);
+// });
+
+
+let $copyBtns = document.querySelectorAll(".js-copyBtn");
+let $copyBtnsArray = Array.from($copyBtns);
+console.log($copyBtnsArray);
+
+copyToClipboard = (event) => {
+  let $thisBtn = event.target;
+  let $thisText = $thisBtn.parentNode.parentNode.querySelector(".js-textToCopy");
+
+  console.log($thisText);
+
+  let $textContent = $thisText.textContent;
+  console.log($textContent);
+	navigator.clipboard.writeText(`${$textContent}`);
+
+	alert(`Text copied to clipboard!`);
+}
+
+$copyBtnsArray.forEach((btn) => {
+  btn.addEventListener("click", copyToClipboard);
 });
+
+
+// handleCopyClick.addEventListener('click', () => {
+// 	let text = codeText.textContent;
+// 	navigator.clipboard.writeText(`${text}`);
+
+// 	alert(`Code copied to clipboard!`);
+// });
+
+// let $accordionToggles = document.querySelectorAll(".js-accordionToggle");
+// let $toggleArray = Array.from($accordionToggles);
+// console.log($toggleArray);
+
+// toggleAccordion = (e) => {
+//   e.preventDefault();
+
+//   let $thisToggle = e.target;
+
+//   let $thisHeader = e.target.parentNode.parentNode;
+
+//   let $thisContent = $thisHeader.nextElementSibling;
+
+//   console.log($thisToggle);
+//   console.log($thisHeader);
+//   console.log($thisContent);
+  
+//   $thisToggle.classList.toggle("is-closed");
+//   $thisHeader.classList.toggle("is-closed");;
+//   $thisContent.classList.toggle("is-closed");
+// };
+
+// $toggleArray.forEach((toggle) => {
+//   toggle.addEventListener("click", toggleAccordion);
+// });
