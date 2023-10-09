00fced4054a4ef6ea7991c60f0ee1f26e16c777a
Kris Kay <kris.kay.design@gmail.com>
2023-10-09 00:32:36 -0400

    git y

--- public/js/content.js
+++ public/js/content.js
@@ -6,10 +6,10 @@
   "use strict";
 
   $(function () {
-    // $("#content").load("/home.html");
+    $("#content").load("/home.html");
     // $("#content").load("/about.html");
     // $("#content").load("/demo.html");
-    $("#content").load("/tutorial.html");
+    // $("#content").load("/tutorial.html");
 
     $("#js-home").on( "click", function(event) {
       event.preventDefault();
--- public/js/copyToClipboard.js
+++ public/js/copyToClipboard.js
@@ -2,17 +2,6 @@
 /* Copy to clipboard */
 /* ================================== */
 
-// let codeText = document.querySelector("#code-text");
-// let handleCopyClick = document.querySelector('#copy-code');
-
-// handleCopyClick.addEventListener('click', () => {
-// 	let text = codeText.textContent;
-// 	navigator.clipboard.writeText(`${text}`);
-
-// 	alert(`Code copied to clipboard!`);
-// });
-
-
 let $copyBtns = document.querySelectorAll(".js-copyBtn");
 let $copyBtnsArray = Array.from($copyBtns);
 console.log($copyBtnsArray);
@@ -33,37 +22,3 @@ copyToClipboard = (event) => {
 $copyBtnsArray.forEach((btn) => {
   btn.addEventListener("click", copyToClipboard);
 });
-
-
-// handleCopyClick.addEventListener('click', () => {
-// 	let text = codeText.textContent;
-// 	navigator.clipboard.writeText(`${text}`);
-
-// 	alert(`Code copied to clipboard!`);
-// });
-
-// let $accordionToggles = document.querySelectorAll(".js-accordionToggle");
-// let $toggleArray = Array.from($accordionToggles);
-// console.log($toggleArray);
-
-// toggleAccordion = (e) => {
-//   e.preventDefault();
-
-//   let $thisToggle = e.target;
-
-//   let $thisHeader = e.target.parentNode.parentNode;
-
-//   let $thisContent = $thisHeader.nextElementSibling;
-
-//   console.log($thisToggle);
-//   console.log($thisHeader);
-//   console.log($thisContent);
-  
-//   $thisToggle.classList.toggle("is-closed");
-//   $thisHeader.classList.toggle("is-closed");;
-//   $thisContent.classList.toggle("is-closed");
-// };
-
-// $toggleArray.forEach((toggle) => {
-//   toggle.addEventListener("click", toggleAccordion);
-// });
