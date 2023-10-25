d45edb2fad115fabf8a4ebab9de1dd70ec33a1bd
Kris Kay <kris.kay.design@gmail.com>
2023-10-24 20:37:18 -0400

    add text size print out script

---
+++ src/js/textSize.js
@@ -0,0 +1,34 @@
+/* ================================== */
+/*  Fluid typography Scale print out */
+/* ================================== */
+
+
+  let textSizeTimeout = false;
+  let textSizeDelay = 300; // delay after event is "complete" to run callback
+
+function textSize() {
+  const $allText = document.querySelectorAll(".js-textSize");
+  const $textArray = Array.from($allText);
+
+  $textArray.forEach((text) => {
+    let $size = window.getComputedStyle(text).getPropertyValue('font-size');
+    let $parsedSize = parseInt($size);
+    // console.log($size);
+    console.log($parsedSize);
+    let $span = text.parentNode.querySelector("span");
+    $span.innerText = `${$parsedSize}`;
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  clearTimeout(textSizeTimeout);
+  timeout = setTimeout(textSize, textSizeDelay);
+// textSize();
+});
+
+window.addEventListener("resize", () =>
+{
+  clearTimeout(textSizeTimeout);
+  timeout = setTimeout(textSize, textSizeDelay);
+// textSize();
+});
