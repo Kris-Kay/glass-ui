5985e0a28f5fbce93ef02c71ccca95836f83ec21
Kris Kay <kris.kay.design@gmail.com>
2024-05-08 20:50:37 -0400

    new accordion look

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,33 +1,22 @@
 /* ========================================== */
-/* Accordion JS */
+/* Accordion */
 /* ========================================== */
+function toggleAccordion(e) {
+  const $thisAccordion = e.target;
+  const $thisToggle = $thisAccordion.querySelector(".js-accordionToggle");
+  const $thisContent = $thisAccordion.querySelector(".js-accordion-content");
+
+  $thisAccordion.classList.toggle("is-closed");
+  $thisToggle.classList.toggle("is-closed");
+  $thisContent.classList.toggle("is-closed");
+};
 
 function findAccordions() {
-  let $accordionToggles = document.querySelectorAll(".js-accordionToggle");
-  let $toggleArray = Array.from($accordionToggles);
-  // console.log($toggleArray);
+  const $accordionsArray = Array.from(document.querySelectorAll(".js-accordion"));
 
-  toggleAccordion = (e) => {
-    e.preventDefault();
-
-    let $thisToggle = e.target;
-    let $thisAccordion = e.target.parentNode;
-    let $thisContent = $thisAccordion.nextElementSibling;
-
-    // console.log($thisToggle);
-    // console.log($thisAccordion);
-    // console.log($thisContent);
-
-    $thisToggle.classList.toggle("is-closed");
-    $thisAccordion.classList.toggle("is-closed");
-    $thisContent.classList.toggle("is-closed");
-  };
-
-  $toggleArray.forEach((toggle) => {
-    toggle.addEventListener("click", toggleAccordion);
+  $accordionsArray.forEach((accordion) => {
+    accordion.addEventListener("click", toggleAccordion);
   });
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  findAccordions();
-});
+/* findAccordions() is called in loadContent.js */
