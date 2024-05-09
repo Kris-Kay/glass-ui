7e66aeefa0ccca414dcb8a01b3232a994abe5816
Kris Kay <kris.kay.design@gmail.com>
2024-05-09 16:28:26 -0400

    set up group and solo accordion

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,14 +1,25 @@
 /* ========================================== */
 /* Accordion */
 /* ========================================== */
+
 function toggleAccordion(e) {
-  const $thisAccordion = e.target;
-  const $thisToggle = $thisAccordion.querySelector(".js-accordionToggle");
+  const $thisBtn = e.target;
+  let $expanded = $thisBtn.getAttribute("aria-expanded") === "true" || false;
+  const $thisAccordion = $thisBtn.parentNode.parentNode;
+  const $thisDown = $thisBtn.querySelector(".js-down");
+  const $thisUp = $thisBtn.querySelector(".js-up");
   const $thisContent = $thisAccordion.querySelector(".js-accordion-content");
 
-  $thisAccordion.classList.toggle("is-closed");
-  $thisToggle.classList.toggle("is-closed");
-  $thisContent.classList.toggle("is-closed");
+  if($thisContent) {
+    $thisBtn.classList.toggle("is-closed");
+    $thisBtn.setAttribute("aria-expanded", !$expanded);
+    // $thisAccordion.classList.toggle("is-closed");
+    $thisDown.classList.toggle("is-closed");
+    $thisUp.classList.toggle("is-closed");
+    $thisContent.classList.toggle("is-closed");
+  } else {
+    console.log("Accordion content not found.");
+  }
 };
 
 function findAccordions() {
