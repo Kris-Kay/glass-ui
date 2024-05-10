802d03c9e1e1ed86caae429afd1967d293501870
Kris Kay <kris.kay.design@gmail.com>
2024-05-09 21:21:02 -0400

    clean up and ani accordion

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -6,16 +6,13 @@ function toggleAccordion(e) {
   const $thisBtn = e.target;
   let $expanded = $thisBtn.getAttribute("aria-expanded") === "true" || false;
   const $thisAccordion = $thisBtn.parentNode.parentNode;
-  const $thisDown = $thisBtn.querySelector(".js-down");
-  const $thisUp = $thisBtn.querySelector(".js-up");
+  const $thisIcon = $thisBtn.querySelector(".js-icon");
   const $thisContent = $thisAccordion.querySelector(".js-accordion-content");
 
   if($thisContent) {
     $thisBtn.classList.toggle("is-closed");
     $thisBtn.setAttribute("aria-expanded", !$expanded);
-    // $thisAccordion.classList.toggle("is-closed");
-    $thisDown.classList.toggle("is-closed");
-    $thisUp.classList.toggle("is-closed");
+    $thisIcon.classList.toggle("is-closed");
     $thisContent.classList.toggle("is-closed");
   } else {
     console.log("Accordion content not found.");
