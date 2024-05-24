5a720c28e2ee0bcf718dcc26efd395c8560ad29d
Kris Kay <kris.kay.design@gmail.com>
2024-05-24 09:11:01 -0400

    clean up tab JS

--- src/js/tabMenu.js
+++ src/js/tabMenu.js
@@ -3,8 +3,8 @@
 /* ========================================== */
 
 function findTabs() {
-  const $tabs = document.querySelectorAll('[role="tab"]');
-  const $tabList = document.querySelector('[role="tablist"]');
+  const $tabList = Array.from(document.querySelectorAll(".js-tabList"));
+  const $tabs = Array.from(document.querySelectorAll(".js-tab"));
 
   // Add a click event handler to each tab
   $tabs.forEach((tab) => {
@@ -14,56 +14,51 @@ function findTabs() {
   // Enable arrow navigation between tabs in the tab list
   let $tabFocus = 0;
 
-  $tabList.addEventListener("keydown", (e) => {
-    // Move right
-    if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
-      // $tabs[$tabFocus].setAttribute("tabindex", -1);
-      if (e.key === "ArrowRight") {
-        $tabFocus++;
-        // If we're at the end, go to the start
-        if ($tabFocus >= $tabs.length) {
-          $tabFocus = 0;
+  $tabList.forEach(($list) => {
+    $list.addEventListener("keydown", (e) => {
+      // Move right
+      if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
+        if (e.key === "ArrowRight") {
+          $tabFocus++;
+          // If we're at the end, go to the start
+          if ($tabFocus >= $tabs.length) {
+            $tabFocus = 0;
+          }
+          // Move left
+        } else if (e.key === "ArrowLeft") {
+          $tabFocus--;
+          // If we're at the start, move to the end
+          if ($tabFocus < 0) {
+            $tabFocus = $tabs.length - 1;
+          }
         }
-        // Move left
-      } else if (e.key === "ArrowLeft") {
-        $tabFocus--;
-        // If we're at the start, move to the end
-        if ($tabFocus < 0) {
-          $tabFocus = $tabs.length - 1;
-        }
+        $tabs[$tabFocus].focus();
       }
-
-      // $tabs[$tabFocus].setAttribute("tabindex", 0);
-      $tabs[$tabFocus].focus();
-    }
+    });
   });
+
+
 };
 
-// document.addEventListener("DOMContentLoaded", () =>
-// {
-//   findTabs();
-// });
 
 function changeTabs(e) {
   const $thisTab = e.target;
   const $thisTabList = $thisTab.parentNode;
   const $thisPanelId = $thisTab.getAttribute("aria-controls");
-  const $thisPanelWrap = document.getElementById(`${$thisPanelId}`).parentNode;
-  const $panels = $thisPanelWrap.querySelectorAll('[role="tabpanel"]');
   const $thisPanel = document.getElementById(`${$thisPanelId}`);
-  // console.log($thisPanel);
+  const $thisPanelGroup = $thisPanel.parentNode;
+  const $thisPanelArray = Array.from($thisPanelGroup.querySelectorAll('[role="tabpanel"]'));
+
   e.preventDefault();
   // Remove all current selected tabs
-  $thisTabList
-    .querySelectorAll('[aria-selected="true"]')
-    .forEach((t) => t.setAttribute("aria-selected", false));
+  $selectedTabs = $thisTabList.querySelectorAll('[aria-selected="true"]')
+  $selectedTabs.forEach((t) => t.setAttribute("aria-selected", false));
 
   // Set this tab as selected
   $thisTab.setAttribute("aria-selected", true);
-  // console.log($thisTab);
 
   // Hide all tab panels
-  $panels.forEach((p) => p.setAttribute("hidden", true));
+  $thisPanelArray.forEach((p) => p.setAttribute("hidden", true));
 
   // Show the selected panel
   $thisPanel.removeAttribute("hidden");
