f1b534a5e00c21a6d534a1ed607c578d2e812bce
Kris Kay <kris.kay.design@gmail.com>
2024-06-06 14:07:43 -0400

    split html

--- src/js/loadContent.js
+++ src/js/loadContent.js
@@ -11,7 +11,7 @@ function callFindFunctions() {
 function fetchHome() {
   const container = document.querySelector('#js-content');
 
-  fetch('/html/home.html')
+  fetch('/html/builder.html')
     .then(response => response.text())
 
     .then(html => {
