98d7d7e0417db7c9ded9e95b28a450c76b6aeae7
Kris Kay <kris.kay.design@gmail.com>
2024-07-16 08:37:00 -0400

    Create fetchContent.js

---
+++ src/js/fetchContent.js
@@ -0,0 +1,75 @@
+/* ========================================== */
+/* Fetch content */
+/* ========================================== */
+const $contentArea = document.querySelector('#js-content');
+const $homeBtns = document.querySelectorAll('.js-homeBtn');
+const $aboutBtns = document.querySelectorAll('.js-aboutBtn');
+const $contactBtns = document.querySelectorAll('.js-contactBtn');
+
+/* Home */
+/* ========================================== */
+function fetchHome() {
+  fetch('/html/generator.html')
+    .then(response => response.text())
+
+    .then(html => {
+      $contentArea.innerHTML = html;
+      findTabs();
+      findSpotlights();
+      findCopyBtns();
+
+      initSliders();
+      // initColorPicker();
+      initializeGenerator();
+    })
+
+    .catch(error => console.error(error.message));
+}
+fetchHome();
+
+/* About */
+/* ========================================== */
+function fetchAbout() {
+  fetch('/html/about.html')
+    .then(response => response.text())
+
+    .then(html => {
+      $contentArea.innerHTML = html;
+      findTabs();
+      findSpotlights();
+      findAccordions();
+    })
+
+    .catch(error => console.error(error.message));
+}
+
+/* Contact */
+/* ========================================== */
+function fetchContact() {
+  fetch('/html/contact.html')
+    .then(response => response.text())
+
+    .then(html => {
+      $contentArea.innerHTML = html;
+      findTabs();
+      findCopyBtns();
+      findSpotlights();
+    })
+
+    .catch(error => console.error(error.message));
+}
+
+
+/* Event Listeners */
+/* ========================================== */
+$homeBtns.forEach(($btn) => {
+  $btn.addEventListener("click", fetchHome);
+});
+
+$aboutBtns.forEach(($btn) => {
+  $btn.addEventListener("click", fetchAbout);
+});
+
+$contactBtns.forEach(($btn) => {
+  $btn.addEventListener("click", fetchContact);
+});
